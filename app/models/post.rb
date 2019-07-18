9888a52024efa7a86a8a4c7497e2a1b0e8e9cf2d
Nataliya R <n.rudenko@gmail.com>
2019-07-18 02:43:11 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validate :click_baity_title, on: :create
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
+    def click_baity_title
+      if self.title && !self.title.match?(/(Won't\sBelieve)|(Secret)|(Top\d+)|(Guess)/)
+        errors.add(:title, "Doesn't have an h")
+      end
+    end
+
 end
