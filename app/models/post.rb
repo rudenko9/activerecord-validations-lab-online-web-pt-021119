264ce2464a1bb455cf24a2f459b2bb8e6061df5a
Nataliya R <n.rudenko@gmail.com>
2019-07-18 02:38:57 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
 validates :title , presence: true
 validates(:content, { :length => { :is => 250 } })
 validates(:summary, { :length => { :maximum => 250 } })
-validates :fiction, inclusion: { in: %w(non-fiction),
-    message: "%{false} " }
+validates :category, inclusion: { in: %w(fiction non-fiction) }
 end
